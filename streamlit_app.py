2bcd35126739b9546f6515bde461171e81e448de
igivis7 <58656086+igivis7@users.noreply.github.com>
2024-04-25 21:47:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,16 +79,30 @@ with tab1:
 # Tab 2
 with tab2:
     st.write("""
-        Welcome to an innovative project designed to enhance maintenance efficiency for manufacturing companies! Our machine learning model, incorporated into a user-friendly web app, predicts maintenance needs in real time by analyzing data from industrial devices. This proactive solution empowers companies to tackle issues before they cause downtime and increased costs.
+        st.subheader('Parameters description')
 
-        The key components of the project include:
-
-        * **Type:** Products are categorized as low (L), medium (M), or high (H) quality variants, each with a specific serial number.
-        * **Air Temperature [K]:** The temperature of the surrounding air, measured in Kelvin.
-        * **Process Temperature [K]:** The temperature of the manufacturing process, measured in Kelvin.
-        * **Rotational Speed [RPM]:** The speed at which the device rotates, measured in revolutions per minute.
-        * **Torque [Nm]:** The applied torque to the device, measured in Newton-meters.
-        * **Tool Wear [min]:** The duration of tool usage, measured in minutes.
+        The dataset consists of the following features:
+        * **Type**: Product quality variant with letters L, M, or H, and a variant-specific serial number.
+        * **Air temperature [K]**: Generated using a random walk process, later normalized to a standard deviation of 2 K around 300 K.
+        * **Process temperature [K]**: Generated using a random walk process, normalized to a standard deviation of 1 K, added to the air temperature plus 10 K.
+        * **Rotational speed [rpm]**: Calculated from power of 2860 W, overlaid with normally distributed noise.
+        * **Torque [Nm]**: Torque values are normally distributed around 40 Nm with an Ïƒ = 10 Nm and no negative values.
+        * **Tool wear [min]**: The quality variants H/M/L add 5/3/2 minutes of tool wear to the used tool in the process.
+        
+        st.subheader('The machine failure modes')
+        
+        If at least one of the above failure modes is true, the process fails and the 'machine failure' label is set to 1. It is therefore not transparent to the machine learning method, which of the failure modes has caused the process to fail 
+        
+        * **tool wear failure (TWF)**: the tool will be replaced of fail at a randomly selected tool wear time between 200 and 240 mins (120 times in our dataset). At this point in time, the tool is replaced 69 times, and fails 51 times (randomly assigned).
+        * **heat dissipation failure (HDF)**: heat dissipation causes a process failure, if the difference between air- and process temperature is below 8.6 K and the tool's rotational speed is below 1380 rpm. This is the case for 115 data points.
+        * **power failure (PWF)**: the product of torque and rotational speed (in rad/s) equals the power required for the process. If this power is below 3500 W or above 9000 W, the process fails, which is the case 95 times in our dataset.
+        * **overstrain failure (OSF)**: if the product of tool wear and torque exceeds 11,000 minNm for the L product variant (12,000 M, 13,000 H), the process fails due to overstrain. This is true for 98 datapoints.
+        * **random failures (RNF)**: each process has a chance of 0,1 % to fail regardless of its process parameters. This is the case for only 5 datapoints, less than could be expected for 10,000 datapoints in our dataset.
+        
+        
+        st.subheader('About the Dataset')
+        
+        The synthetic dataset provided in this application reflects real predictive maintenance encountered in the industry to the best of our knowledge. The dataset contains 10,000 data points with 14 features. It includes a mix of low, medium, and high-quality variants, each with a specific serial number. The features represent various parameters like air temperature, process temperature, rotational speed, torque, and tool wear.
 
         To explore the code and understand how our solution automates this process effectively, check out the project on [Kaggle](https://www.kaggle.com/code/atom1991/optimizing-operations-with-predictive-maintenance?kernelSessionId=146948811).
     """)
