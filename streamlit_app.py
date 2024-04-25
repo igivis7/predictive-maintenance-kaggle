8a0673409fdcb640cbb32cc033bbe3476545595e
igivis7 <58656086+igivis7@users.noreply.github.com>
2024-04-25 21:33:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,33 +32,8 @@ def user_input_features():
 input_df = user_input_features()
 input_df_copy = input_df.copy()
 
-tab1 = st.tabs([' Maintenance Prediction'])
-# Tab 1
-with tab1:
-    st.write("Welcome to the Predictive Maintenance for Industrial Devices web application! This web app predicts maintenance needs by analyzing data from industrial devices, ensuring proactive and efficient operations.")
-    st.info('Adjust the sliders or select values in the **sidebar** to input essential operational data', icon="ℹ️")
-    st.subheader('Input Details')
-    st.write(f"""
-            * **Type:** {input_df['Type'].values[0]}
-            * **Air Temperature:** {input_df['Air Temperature'].values[0]} K
-            * **Process Temperature:** {input_df['Process Temperature'].values[0]} K
-            * **Rotational Speed:** {input_df['Rotational Speed'].values[0]} RPM
-            * **Torque:** {input_df['Torque'].values[0]} Nm
-            * **Tool Wear:** {input_df['Tool wear'].values[0]} min
-            """)
 
-# Feature Engineering
-input_df['Power'] = 2 * np.pi * input_df['Rotational Speed'] * input_df['Torque'] / 60
-input_df['temp_diff'] = input_df['Process Temperature'] - input_df['Air Temperature']
-input_df['Type_H'] = 0
-input_df['Type_L'] = 0
-input_df['Type_M'] = 0
-if input_df['Type'].values == 'L':
-    input_df['Type_L'] = 1
-elif input_df['Type'].values == 'M':
-    input_df['Type_M'] = 1
-else:
-    input_df['Type_H'] = 1
+
 
 input_df = input_df.drop(['Type', 'Air Temperature', 'Process Temperature', 'Rotational Speed', 'Torque'], axis = 1)
 
@@ -66,15 +41,14 @@ model = joblib.load("predictive_maintenance.pkl")
 prediction = model.predict(input_df)
 prediction_probability = model.predict_proba(input_df)
 
-# Tab 1
-with tab1:
-    st.subheader('Prediction')
-    if prediction == 0:
-        st.success('No Maintenance Required', icon="✅")
-        st.write(f"Probability: **{list(prediction_probability)[0][0]:.2%}**")
-    else:
-        st.error('Maintenance Needed', icon="🚨")
-        st.write(f"Probability: **{list(prediction_probability)[0][1]:.2%}**")
+
+st.subheader('Prediction')
+if prediction == 0:
+    st.success('No Maintenance Required', icon="✅")
+    st.write(f"Probability: **{list(prediction_probability)[0][0]:.2%}**")
+else:
+    st.error('Maintenance Needed', icon="🚨")
+    st.write(f"Probability: **{list(prediction_probability)[0][1]:.2%}**")
 
 
 
