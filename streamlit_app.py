21a8ce5e186f6dfed6bc525e9ce39f7fa94f0726
igivis7 <58656086+igivis7@users.noreply.github.com>
2024-04-25 21:49:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,9 +78,9 @@ with tab1:
 
 # Tab 2
 with tab2:
+    
+    st.subheader('Parameters description')
     st.write("""
-        st.subheader('Parameters description')
-
         The dataset consists of the following features:
         * **Type**: Product quality variant with letters L, M, or H, and a variant-specific serial number.
         * **Air temperature [K]**: Generated using a random walk process, later normalized to a standard deviation of 2 K around 300 K.
@@ -88,9 +88,10 @@ with tab2:
         * **Rotational speed [rpm]**: Calculated from power of 2860 W, overlaid with normally distributed noise.
         * **Torque [Nm]**: Torque values are normally distributed around 40 Nm with an Ïƒ = 10 Nm and no negative values.
         * **Tool wear [min]**: The quality variants H/M/L add 5/3/2 minutes of tool wear to the used tool in the process.
-        
-        st.subheader('The machine failure modes')
-        
+    """)
+    
+    st.subheader('The machine failure modes')
+    st.write("""
         If at least one of the above failure modes is true, the process fails and the 'machine failure' label is set to 1. It is therefore not transparent to the machine learning method, which of the failure modes has caused the process to fail 
         
         * **tool wear failure (TWF)**: the tool will be replaced of fail at a randomly selected tool wear time between 200 and 240 mins (120 times in our dataset). At this point in time, the tool is replaced 69 times, and fails 51 times (randomly assigned).
@@ -98,12 +99,12 @@ with tab2:
         * **power failure (PWF)**: the product of torque and rotational speed (in rad/s) equals the power required for the process. If this power is below 3500 W or above 9000 W, the process fails, which is the case 95 times in our dataset.
         * **overstrain failure (OSF)**: if the product of tool wear and torque exceeds 11,000 minNm for the L product variant (12,000 M, 13,000 H), the process fails due to overstrain. This is true for 98 datapoints.
         * **random failures (RNF)**: each process has a chance of 0,1 % to fail regardless of its process parameters. This is the case for only 5 datapoints, less than could be expected for 10,000 datapoints in our dataset.
-        
-        
-        st.subheader('About the Dataset')
-        
+    """)
+    
+    st.subheader('About the Dataset')
+    st.write("""
         The synthetic dataset provided in this application reflects real predictive maintenance encountered in the industry to the best of our knowledge. The dataset contains 10,000 data points with 14 features. It includes a mix of low, medium, and high-quality variants, each with a specific serial number. The features represent various parameters like air temperature, process temperature, rotational speed, torque, and tool wear.
-
+    
         To explore the code and understand how our solution automates this process effectively, check out the project on [Kaggle](https://www.kaggle.com/code/atom1991/optimizing-operations-with-predictive-maintenance?kernelSessionId=146948811).
     """)
 
